24345a916e54a9a63290156d27b6307ea0461e2f
Abhishek Jha <jhaabhishek812t@gmail.com>
2022-07-07 22:07:27 +0530

    Add files via upload

---
+++ pokemon/style.js
@@ -0,0 +1,7 @@
+let btn = document.querySelector('button');
+btn.addEventListener('click', fn);
+
+function fn() {
+    let name = confirm('Are you Sure You want to view image?');
+    window.location.href = "images.html";
+}
