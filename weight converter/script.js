05f643b37a8d3de8cbe8f02c3db531ae12a39516
Abhishek Jha <jhaabhishek812t@gmail.com>
2022-07-25 00:59:37 +0530

    Delete weight converter directory

--- weight converter/script.js
+++
@@ -1,8 +0,0 @@
-function convert() {
-    var input = document.getElementById('wt').value;
-    if (input == '') alert("Please enter input");
-    else {
-        var output = input / 1000.0;
-        document.getElementById('result').innerHTML = `${output} kg`;
-    }
-}
